cf215bf6d4239dfc2efce0a362b6f551f0ce813e
jrmwng <jmw@netvigator.com>
2017-07-27 22:31:57 +0800

    add implementations for 'cmpeq', 'char16_t' and 'char32_t'

--- like2017/simd_string.h
+++ like2017/simd_string.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <intrin.h>
-#include <string>
 
 namespace jrmwng
 {
@@ -20,17 +19,23 @@ namespace jrmwng
 			{}
 			simd_string(char const *pc)
 			{
-				strncpy_s(m_xmm.m128i_i8, 16, pc, 15);
+				std::char_traits<char>::_Copy_s(m_xmm.m128i_i8, 16, pc, 15);
 			}
 
 			int cmplt(simd_string const & that) const
 			{
-				__m128i const b16This = _mm_add_epi8(_mm_set1_epi8(-128), this->m_xmm);
-				__m128i const b16That = _mm_add_epi8(_mm_set1_epi8(-128), that.m_xmm);
+				__m128i const b16This = _mm_add_epi8(_mm_set1_epi8(-0x80), this->m_xmm);
+				__m128i const b16That = _mm_add_epi8(_mm_set1_epi8(-0x80), that.m_xmm);
 				__m128i const b16CmpLt = _mm_cmplt_epi8(b16This, b16That);
 				int const nCmpLt = _mm_movemask_epi8(b16CmpLt);
 				int const nTzCnt = _tzcnt_u32(nCmpLt);
 				return nTzCnt;
+			}
+			int cmpeq(simd_string const & that) const
+			{
+				__m128i const b16That = that.m_xmm;
+				__m128i const b16This = m_xmm;
+				return _mm_cmpistrc(b16That, b16This, _SIDD_UBYTE_OPS | _SIDD_CMP_EQUAL_EACH | _SIDD_NEGATIVE_POLARITY | _SIDD_LEAST_SIGNIFICANT);
 			}
 
 			operator char const * () const
@@ -42,44 +47,111 @@ namespace jrmwng
 			{
 				__m128i const b1Pattern = _mm_cvtsi32_si128(cPattern);
 				__m128i const b16This = m_xmm;
-				int const nIndex = _mm_cmpestri(b1Pattern, 1, b16This, 16, _SIDD_UBYTE_OPS | _SIDD_CMP_EQUAL_ANY | _SIDD_POSITIVE_POLARITY | _SIDD_LEAST_SIGNIFICANT);
+				int const nIndex = _mm_cmpistri(b1Pattern, b16This, _SIDD_UBYTE_OPS | _SIDD_CMP_EQUAL_ORDERED | _SIDD_POSITIVE_POLARITY | _SIDD_LEAST_SIGNIFICANT);
+				return nIndex;
+			}
+			int index_of(simd_string const & that) const
+			{
+				__m128i const b16That = that.m_xmm;
+				__m128i const b16This = m_xmm;
+				int const nIndex = _mm_cmpistri(b16That, b16This, _SIDD_UBYTE_OPS | _SIDD_CMP_EQUAL_ORDERED | _SIDD_POSITIVE_POLARITY | _SIDD_LEAST_SIGNIFICANT);
 				return nIndex;
 			}
 		};
 		template <>
-		struct simd_string<__m128i, wchar_t>
+		struct simd_string<__m128i, char16_t>
 		{
 			__m128i m_xmm;
 
 			simd_string(simd_string const & that)
 				: m_xmm(that.m_xmm)
 			{}
-			simd_string(wchar_t const *pc)
+			simd_string(char16_t const *pc)
 				: m_xmm(_mm_setzero_si128())
 			{
-				wcsncpy_s(reinterpret_cast<wchar_t*>(m_xmm.m128i_u16), 8, pc, 7);
+				std::char_traits<char16_t>::_Copy_s(reinterpret_cast<char16_t*>(m_xmm.m128i_i16), 8, pc, 7);
 			}
 
 			int cmplt(simd_string const & that) const
 			{
-				__m128i const w8This = _mm_add_epi16(_mm_set1_epi16(-32768), this->m_xmm);
-				__m128i const w8That = _mm_add_epi16(_mm_set1_epi16(-32768), that.m_xmm);
+				__m128i const w8This = _mm_add_epi16(_mm_set1_epi16(-0x8000), this->m_xmm);
+				__m128i const w8That = _mm_add_epi16(_mm_set1_epi16(-0x8000), that.m_xmm);
 				__m128i const w8CmpLt = _mm_cmplt_epi16(w8This, w8That);
 				int const nCmpLt = _mm_movemask_epi8(w8CmpLt);
 				int const nTzCnt = _tzcnt_u32(nCmpLt);
 				return nTzCnt;
 			}
-
-			operator wchar_t const * () const
+			int cmpeq(simd_string const & that) const
 			{
-				return reinterpret_cast<wchar_t const*>(m_xmm.m128i_i16);
+				__m128i const b16That = that.m_xmm;
+				__m128i const b16This = m_xmm;
+				return _mm_cmpistrc(b16That, b16This, _SIDD_UWORD_OPS | _SIDD_CMP_EQUAL_EACH | _SIDD_NEGATIVE_POLARITY | _SIDD_LEAST_SIGNIFICANT);
 			}
 
-			int index_of(wchar_t cPattern) const
+			operator char16_t const * () const
+			{
+				return reinterpret_cast<char16_t const*>(m_xmm.m128i_i16);
+			}
+
+			int index_of(char16_t cPattern) const
 			{
 				__m128i const uw1Pattern = _mm_cvtsi32_si128(cPattern);
 				__m128i const uw8This = m_xmm;
-				int const nIndex = _mm_cmpestri(uw1Pattern, 1, uw8This, 8, _SIDD_UWORD_OPS | _SIDD_CMP_EQUAL_ANY | _SIDD_POSITIVE_POLARITY | _SIDD_LEAST_SIGNIFICANT);
+				int const nIndex = _mm_cmpistri(uw1Pattern, uw8This, _SIDD_UWORD_OPS | _SIDD_CMP_EQUAL_ORDERED | _SIDD_POSITIVE_POLARITY | _SIDD_LEAST_SIGNIFICANT);
+				return nIndex;
+			}
+			int index_of(simd_string const & that) const
+			{
+				__m128i const b16That = that.m_xmm;
+				__m128i const b16This = m_xmm;
+				int const nIndex = _mm_cmpistri(b16That, b16This, _SIDD_UWORD_OPS | _SIDD_CMP_EQUAL_ORDERED | _SIDD_POSITIVE_POLARITY | _SIDD_LEAST_SIGNIFICANT);
+				return nIndex;
+			}
+		};
+		template <>
+		struct simd_string<__m128i, char32_t>
+		{
+			__m128i m_xmm;
+
+			simd_string(simd_string const & that)
+				: m_xmm(that.m_xmm)
+			{}
+			simd_string(char32_t const *pc)
+				: m_xmm(_mm_setzero_si128())
+			{
+				std::char_traits<char32_t>::_Copy_s(reinterpret_cast<char32_t*>(m_xmm.m128i_i32), 4, pc, 3);
+			}
+
+			int cmplt(simd_string const & that) const
+			{
+				__m128i const l4This = _mm_add_epi32(_mm_set1_epi32(-0x80000000LL), this->m_xmm);
+				__m128i const l4That = _mm_add_epi32(_mm_set1_epi32(-0x80000000LL), that.m_xmm);
+				__m128i const l4CmpLt = _mm_cmplt_epi32(l4This, l4That);
+				int const nCmpLt = _mm_movemask_epi8(l4CmpLt);
+				int const nTzCnt = _tzcnt_u32(nCmpLt);
+				return nTzCnt;
+			}
+			bool cmpeq(simd_string const & that) const
+			{
+				__m128i const l4This = this->m_xmm;
+				__m128i const l4That = that.m_xmm;
+				__m128i const l4CmpEq = _mm_cmpeq_epi32(l4This, l4That);
+				int const nCmpEq = _mm_movemask_epi8(l4CmpEq);
+				return nCmpEq == 0xFFFF;
+			}
+
+			operator char32_t const * () const
+			{
+				return reinterpret_cast<char32_t const*>(m_xmm.m128i_i32);
+			}
+
+			int index_of(char32_t cPattern) const
+			{
+				__m128i const l4Pattern = _mm_set1_epi32(cPattern);
+				__m128i const l4This = m_xmm;
+				__m128i const l4CmpEq = _mm_cmpeq_epi32(l4Pattern, l4This);
+				int const nCmpEq = _mm_movemask_ps(_mm_castsi128_ps(l4CmpEq));
+				int const nIndex = _tzcnt_u32(nCmpEq);
 				return nIndex;
 			}
 		};
@@ -94,17 +166,25 @@ namespace jrmwng
 			simd_string(char const *pc)
 				: m_ymm(_mm256_setzero_si256())
 			{
-				strncpy_s(m_ymm.m256i_i8, pc, 31);
+				std::char_traits<char>::_Copy_s(m_ymm.m256i_i8, 32, pc, 31);
 			}
 
 			int cmplt(simd_string const & that) const
 			{
-				__m256i const b32This = _mm256_add_epi8(_mm256_set1_epi8(-128), this->m_ymm);
-				__m256i const b32That = _mm256_add_epi8(_mm256_set1_epi8(-128), that.m_ymm);
+				__m256i const b32This = _mm256_add_epi8(_mm256_set1_epi8(-0x80), this->m_ymm);
+				__m256i const b32That = _mm256_add_epi8(_mm256_set1_epi8(-0x80), that.m_ymm);
 				__m256i const b32CmpLt = _mm256_cmpgt_epi8(b32That, b32This);
 				int const nCmpLt = _mm256_movemask_epi8(b32CmpLt);
 				int const nTzCnt = _tzcnt_u32(nCmpLt);
 				return nTzCnt;
+			}
+			bool cmpeq(simd_string const & that) const
+			{
+				__m256i const b32This = this->m_ymm;
+				__m256i const b32That = that.m_ymm;
+				__m256i const b32CmpEq = _mm256_cmpeq_epi8(b32This, b32That);
+				int const nCmpEq = _mm256_movemask_epi8(b32CmpEq);
+				return ~nCmpEq == 0;
 			}
 
 			operator char const * () const
@@ -166,9 +246,10 @@ namespace jrmwng
 			return base_t::operator Tchar const *();
 		}
 
-		int index_of(Tchar cPattern) const
+		template <typename Targ>
+		int index_of(Targ && arg) const
 		{
-			return base_t::index_of(cPattern);
+			return base_t::index_of(std::forward<Targ>(arg));
 		}
 		int length() const
 		{
